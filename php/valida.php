3cbc0a2035a209ff4cf543eb485a34bf88228bc5
Maicon Bruno <maiconbrunorodrigues@gmail.com>
2019-06-04 14:13:19 -0300

    Comentando todo a pagina valida.php validaPagina.php .

--- php/valida.php
+++ php/valida.php
@@ -1,32 +1,32 @@
 <?php
 session_start();	
 include ('./conexao.php');
-
+//Captura as informações digitadas no input text do html da pagina de login 
 $usuario = mysqli_real_escape_string($conn, $_POST['login']); //Escapar de caracteres especiais, como aspas, prevenindo SQL injection
-$senha = mysqli_real_escape_string($conn, $_POST['senha']);
+$senha = mysqli_real_escape_string($conn, $_POST['senha']);//Escapar de caracteres especiais, como aspas, prevenindo SQL injection
 
 
 
- if(($usuario != null) && ($senha != null)){
+ if(($usuario != null) && ($senha != null)){  // Verifica se os campos foram preenchidos, caso não redireciona para pagina index.php e exibi a mensagem de alerta!
         
-        $result_usuario = "call pfc_db.select_valida_usuario('$usuario','$senha');";
-		$resultado_usuario = mysqli_query($conn, $result_usuario);
-        $resultado = mysqli_fetch_assoc($resultado_usuario);
+        $result_usuario = "call pfc_db.select_valida_usuario('$usuario','$senha');"; //Procedure que captura os dados do banco para comparação.
+		$resultado_usuario = mysqli_query($conn, $result_usuario); // faz a conexão com banco e aloca o resultado na variavel resultado_usuario 
+        $resultado = mysqli_fetch_assoc($resultado_usuario); // Associa o resultado com o banco 
         
-        if($resultado != null){
-			$_SESSION['usuarioId'] = $resultado['id_funcionario'];
-			$_SESSION['usuarioNome'] = $resultado['nome'];
-			$_SESSION['usuarioNiveisAcessoId'] = $resultado['funcao'];
-			$_SESSION['usuarioUsuario'] = $resultado['usuario'];
+        if($resultado != null){ // Se o resultado for vazio ele volta para pagina de login se não for ele cria uma sessão com os dados. 
+			$_SESSION['usuarioId'] = $resultado['id_funcionario']; //Guarda uma sessão com ID do usuario logado
+			$_SESSION['usuarioNome'] = $resultado['nome']; //Guarda em uma sessão o nome do usuario logado
+			$_SESSION['usuarioNiveisAcessoId'] = $resultado['funcao'];//Guarda em uma sessão o nivel do usuario logado
+			$_SESSION['usuarioUsuario'] = $resultado['usuario'];//Guarda em uma sessão o login do usuario 
 			
-			if($_SESSION['usuarioNiveisAcessoId'] == "1"){
-				$_SESSION['Logado'] = true; 
+			if($_SESSION['usuarioNiveisAcessoId'] == "1"){// verifica o nivel e direciona para pagina correspondente 
+				$_SESSION['Logado'] = true; // Cria a sessão que Valida o login 
 				header("Location: ../PaginaGraficos.php");
 			}elseif($_SESSION['usuarioNiveisAcessoId'] == "2"){
-				$_SESSION['Logado'] = true; 
+				$_SESSION['Logado'] = true;  // Cria a sessão que Valida o login 
 				header("Location: ../PaginaDoAgente.php"); //Colocar Url da paginas Relacionadas ao usuario Agente
 			}else{
-				$_SESSION['Logado'] = true; 
+				$_SESSION['Logado'] = true;  // Cria a sessão que Valida o login 
 				header("Location: ../PaginaSupervisor.php"); //Colocar Url da paginas Relacionadas ao usuario supervisor
 			}
 		//Não foi encontrado um usuario na tabela usuário com os mesmos dados digitado no formulário
@@ -38,6 +38,7 @@ $senha = mysqli_real_escape_string($conn, $_POST['senha']);
 		}
 
  }else {
+	 //Váriavel global recebendo a mensagem de erro
     $_SESSION['loginErro'] = "Usuário ou senha inválido";
     header("Location: ../index.php");
  }
--- php/validaPagina.php
+++ php/validaPagina.php
@@ -1,11 +1,12 @@
 <?php
+//Starta a sessão da pagina. 
 session_start();
 
 //verifica se a sessão existe, se não existir ele redireciona para pagina de login
-if (isset($_SESSION['usuarioId']) === true) {
- 
- } else {
+if (isset($_SESSION['usuarioId']) === true) { //não faz nada caso a sessão exista
+   
+ } else { //Caso ela n exista redireciona para pagina de login
   header("Location: index.php");
-  exit();
+  exit(); // interrompe o funcionamento da pagina para não prosseguir com a leitura do codigo. 
 }
 
