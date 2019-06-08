07f9852e9b31157a3a2feaa918be1f253d518f90
Andrew <maiconbrunorodrigues@gmail.com>
2019-06-08 17:34:17 -0300

    TabelaExport PDF 08.06

--- TabelaGeraldoAdministrador.php
+++ TabelaGeraldoAdministrador.php
@@ -59,6 +59,12 @@ include('./php/tabelaavaliacao.php');
     <link href="css/scrolling-nav.css" rel="stylesheet">
     <!-- import estilos.css -->
 
+    <!-- ImportarPDF -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.4.1/jspdf.min.js"></script> 
+    <script src="https://cdn.jsdelivr.net/npm/canvas2image@1.0.5/canvas2image.min.js"></script>
+
+
 </head>
 
 <body>
@@ -217,6 +223,8 @@ include('./php/tabelaavaliacao.php');
                                     </tbody>
                                 </table>
 
+                                <input type="button" value="PDF" id="idPDF" onclick="createPDF();">
+
                                 <script>
                                     $(document).ready(function() {
                                         $('#tbl_ligacao').DataTable({
@@ -269,6 +277,75 @@ include('./php/tabelaavaliacao.php');
     <script src="//code.jquery.com/jquery-3.2.1.min.js"></script>
     <script src="//cdn.datatables.net/1.10.15/js/jquery.dataTables.min.js"></script>
     <script src="//cdn.datatables.net/plug-ins/1.10.19/sorting/date-eu.js"></script>
+
+
+
+
+
+    
+
+
+    
+<!-- Importar PDF -->
+    <script>
+    (function($){
+    $.fn.createPdf = function(parametros) {
+        
+        var config = {              
+            'fileName':'html-to-pdf'
+        };
+        
+        if (parametros){
+            $.extend(config, parametros);
+        }                            
+
+        var quotes = document.getElementById($(this).attr('id'));
+
+        html2canvas(quotes, {
+            onrendered: function(canvas) {
+                var pdf = new jsPDF('p', 'pt', 'letter');
+
+                for (var i = 0; i <= quotes.clientHeight/980; i++) {
+                    var srcImg  = canvas;
+                    var sX      = 0;
+                    var sY      = 980*i;
+                    var sWidth  = 900;
+                    var sHeight = 980;
+                    var dX      = 0;
+                    var dY      = 0;
+                    var dWidth  = 900;
+                    var dHeight = 980;
+
+                    window.onePageCanvas = document.createElement("canvas");
+                    onePageCanvas.setAttribute('width', 900);
+                    onePageCanvas.setAttribute('height', 980);
+                    var ctx = onePageCanvas.getContext('2d');
+                    ctx.drawImage(srcImg,sX,sY,sWidth,sHeight,dX,dY,dWidth,dHeight);
+
+                    var canvasDataURL = onePageCanvas.toDataURL("image/png", 1.0);
+                    var width         = onePageCanvas.width;
+                    var height        = onePageCanvas.clientHeight;
+
+                    if (i > 0) {
+                        pdf.addPage(612, 791);
+                    }
+
+                    pdf.setPage(i+1);
+                    pdf.addImage(canvasDataURL, 'PNG', 20, 40, (width*.62), (height*.62));
+                }
+
+                pdf.save(config.fileName);
+            }
+        });
+    };
+})(jQuery);
+
+function createPDF() {
+    $('#tbl_ligacao').createPdf({
+        'fileName' : 'testePDF'
+    });
+};
+</script>
 
 </body>
 
