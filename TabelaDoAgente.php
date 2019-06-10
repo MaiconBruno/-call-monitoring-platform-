d6a72cde94614c741ae14f55b69dfe5b9721a6d2
netohelvecio <helvecioneto77@gmail.com>
2019-06-09 23:20:35 -0300

    gerador de relatorios melhorado + campo de pesquisa tbl_agente

--- TabelaDoAgente.php
+++ TabelaDoAgente.php
@@ -112,7 +112,7 @@ include('./php/tabelaAvaliacaoAgente.php');
                             <!-- Div Esquerda que aloca 2 graficos -->
 
                             <!-- Card principal -->
-                            <div class="text-dark col-md-12 col-sm-12 com-xs-12">
+                            <div class="text-dark col-md-12 col-sm-12 com-xs-12" id="divTable">
                                 <table class="table table-hover table-primary table-responsive-lg" id="tbl_ligacao">
                                     <thead>
                                         <tr class=>
@@ -167,31 +167,29 @@ include('./php/tabelaAvaliacaoAgente.php');
                                         <?php }  ?>
                                     </tbody>
                                 </table>
-
-                                <button style="border-radius:10px;" type="button" class="btn btn-white zoom p-3" id="idPDF" onclick="createPDF();">
-                                    <p class="text-center text-danger">Exportar</p><img width="50" height="50" src="./icones/pdficon.png">
-                                </button>
-                                <button style="border-radius:10px;" type="button" class="btn btn-white zoom p-3" id="idExcell" onclick="createPDF();">
-                                    <p class="text-center text-success">Exportar</p><img width="50" height="50" src="./icones/excellicon.png">
-                                </button>
-                                <script>
-                                    $(document).ready(function() {
-                                        $('#tbl_ligacao').DataTable({
-                                            "searching": false,
-                                            "language": {
-                                                "url": "//cdn.datatables.net/plug-ins/1.10.19/i18n/Portuguese-Brasil.json",
-                                            },
-                                            columnDefs: [{
-                                                type: 'date-eu',
-                                                targets: 4
-                                            }]
-                                        });
-                                    });
-                                </script>
-
                                 <!-- Aqui vai a tabela
                              -->
                             </div>
+
+                            <button style="border-radius:10px;" type="button" class="btn btn-white zoom p-3" id="idPDF" onclick="createPDF();">
+                                <p class="text-center text-danger">Exportar</p><img width="50" height="50" src="./icones/pdficon.png">
+                                </button>
+                            <button style="border-radius:10px;" type="button" class="btn btn-white zoom p-3" id="idExcell" onclick="createPDF();">
+                                <p class="text-center text-success">Exportar</p><img width="50" height="50" src="./icones/excellicon.png">
+                            </button>
+                            <script>
+                                $(document).ready(function() {
+                                    $('#tbl_ligacao').DataTable({
+                                        "language": {
+                                            "url": "//cdn.datatables.net/plug-ins/1.10.19/i18n/Portuguese-Brasil.json",
+                                        },
+                                        columnDefs: [{
+                                            type: 'date-eu',
+                                            targets: 4
+                                        }]
+                                    });
+                                });
+                            </script>
                         </div>
                     </div>
                     <!-- Fim do card principal -->
@@ -236,65 +234,19 @@ include('./php/tabelaAvaliacaoAgente.php');
 
 
     <!-- Importar PDF -->
-    <script>
-        (function($) {
-            $.fn.createPdf = function(parametros) {
-
-                var config = {
-                    'fileName': 'html-to-pdf'
-                };
-
-                if (parametros) {
-                    $.extend(config, parametros);
-                }
-
-                var quotes = document.getElementById($(this).attr('id'));
-
-                html2canvas(quotes, {
-                    onrendered: function(canvas) {
-                        var pdf = new jsPDF('p', 'pt', 'letter');
-
-                        for (var i = 0; i <= quotes.clientHeight / 980; i++) {
-                            var srcImg = canvas;
-                            var sX = 0;
-                            var sY = 980 * i;
-                            var sWidth = 900;
-                            var sHeight = 980;
-                            var dX = 0;
-                            var dY = 0;
-                            var dWidth = 900;
-                            var dHeight = 980;
-
-                            window.onePageCanvas = document.createElement("canvas");
-                            onePageCanvas.setAttribute('width', 900);
-                            onePageCanvas.setAttribute('height', 980);
-                            var ctx = onePageCanvas.getContext('2d');
-                            ctx.drawImage(srcImg, sX, sY, sWidth, sHeight, dX, dY, dWidth, dHeight);
-
-                            var canvasDataURL = onePageCanvas.toDataURL("image/png", 1.0);
-                            var width = onePageCanvas.width;
-                            var height = onePageCanvas.clientHeight;
-
-                            if (i > 0) {
-                                pdf.addPage(612, 791);
-                            }
-
-                            pdf.setPage(i + 1);
-                            pdf.addImage(canvasDataURL, 'PNG', 20, 40, (width * .62), (height * .62));
-                        }
-
-                        pdf.save(config.fileName);
-                    }
-                });
-            };
-        })(jQuery);
-
-        function createPDF() {
-            $('#tbl_ligacao').createPdf({
-                'fileName': 'testePDF'
-            });
-        };
-    </script>
+<script>
+var doc = new jsPDF({
+        orientation: 'l',
+        unit: 'pt',
+        format: [1000, 2200]
+    });
+    $('#idPDF').click(function () {
+        doc.fromHTML($('#divTable').html(), 30, 30, {
+            'width': 100,
+        });
+        doc.save('TabelaLigacoes.pdf');
+    });
+</script>
 
 </body>
 
--- TabelaGeraldoSupervisor.php
+++ TabelaGeraldoSupervisor.php
@@ -171,61 +171,62 @@ include('./php/tabelaavaliacao.php');
                                         <!-- FIm do campos text/date -->
                                     </form>
                                 </div>
-                                <table class="table table-hover table-primary table-responsive-lg" id="tbl_ligacao">
-                                    <thead>
-                                        <tr class=>
-                                            <th>Matricula</th>
-                                            <th>Agente</th>
-                                            <th>Contato do Cliente</th>
-                                            <th>Função</th>
-                                            <th>Data</th>
-                                            <th>Campanha</th>
-                                            <th>Equipe</th>
-                                            <th>R1</th>
-                                            <th>R2</th>
-                                            <th>R3</th>
-                                            <th>R4</th>
-                                            <th>R5</th>
-                                        </tr>
-                                    </thead>
-
-                                    <tbody>
-                                        <?php while ($dado = $resultado->fetch_array()) {
-                                            if ($dado['resposta_1'] == null) {
-                                                $dado['resposta_1'] = "N/A";
-                                            }
-                                            if ($dado['resposta_2'] == null) {
-                                                $dado['resposta_2'] = "N/A";
-                                            }
-                                            if ($dado['resposta_3'] == null) {
-                                                $dado['resposta_3'] = "N/A";
-                                            }
-                                            if ($dado['resposta_4'] == null) {
-                                                $dado['resposta_4'] = "N/A";
-                                            }
-                                            if ($dado['resposta_5'] == null) {
-                                                $dado['resposta_5'] = "N/A";
-                                            }
-                                            ?>
-
-                                            <tr>
-                                                <td><?php echo $dado['matricula']; ?></td>
-                                                <td><?php echo $dado['nome']; ?></td>
-                                                <td><?php echo $dado['ani']; ?></td>
-                                                <td><?php echo $dado['descricao']; ?></td>
-                                                <td><?php echo $dado['data_hora']; ?></td>
-                                                <td><?php echo $dado['campanha']; ?></td>
-                                                <td><?php echo $dado['equipe']; ?></td>
-                                                <td><?php echo $dado['resposta_1']; ?></td>
-                                                <td><?php echo $dado['resposta_2']; ?></td>
-                                                <td><?php echo $dado['resposta_3']; ?></td>
-                                                <td><?php echo $dado['resposta_4']; ?></td>
-                                                <td><?php echo $dado['resposta_5']; ?></td>
+                                <div id="divTable">
+                                    <table class="table table-hover table-primary table-responsive-lg" id="tbl_ligacao">
+                                        <thead>
+                                            <tr class=>
+                                                <th>Matricula</th>
+                                                <th>Agente</th>
+                                                <th>Contato do Cliente</th>
+                                                <th>Função</th>
+                                                <th>Data</th>
+                                                <th>Campanha</th>
+                                                <th>Equipe</th>
+                                                <th>R1</th>
+                                                <th>R2</th>
+                                                <th>R3</th>
+                                                <th>R4</th>
+                                                <th>R5</th>
                                             </tr>
-                                        <?php }  ?>
-                                    </tbody>
-                                </table>
+                                        </thead>
 
+                                        <tbody>
+                                            <?php while ($dado = $resultado->fetch_array()) {
+                                                if ($dado['resposta_1'] == null) {
+                                                    $dado['resposta_1'] = "N/A";
+                                                }
+                                                if ($dado['resposta_2'] == null) {
+                                                    $dado['resposta_2'] = "N/A";
+                                                }
+                                                if ($dado['resposta_3'] == null) {
+                                                    $dado['resposta_3'] = "N/A";
+                                                }
+                                                if ($dado['resposta_4'] == null) {
+                                                    $dado['resposta_4'] = "N/A";
+                                                }
+                                                if ($dado['resposta_5'] == null) {
+                                                    $dado['resposta_5'] = "N/A";
+                                                }
+                                                ?>
+
+                                                <tr>
+                                                    <td><?php echo $dado['matricula']; ?></td>
+                                                    <td><?php echo $dado['nome']; ?></td>
+                                                    <td><?php echo $dado['ani']; ?></td>
+                                                    <td><?php echo $dado['descricao']; ?></td>
+                                                    <td><?php echo $dado['data_hora']; ?></td>
+                                                    <td><?php echo $dado['campanha']; ?></td>
+                                                    <td><?php echo $dado['equipe']; ?></td>
+                                                    <td><?php echo $dado['resposta_1']; ?></td>
+                                                    <td><?php echo $dado['resposta_2']; ?></td>
+                                                    <td><?php echo $dado['resposta_3']; ?></td>
+                                                    <td><?php echo $dado['resposta_4']; ?></td>
+                                                    <td><?php echo $dado['resposta_5']; ?></td>
+                                                </tr>
+                                            <?php }  ?>
+                                        </tbody>
+                                    </table>
+                                </div>
                                 <button style="border-radius:10px;" type="button" class="btn btn-white zoom p-3" id="idPDF" onclick="createPDF();">
                                     <p class="text-center text-danger">Exportar</p><img width="50" height="50" src="./icones/pdficon.png">
                                 </button>
@@ -295,64 +296,18 @@ include('./php/tabelaavaliacao.php');
 
     
 <!-- Importar PDF -->
-    <script>
-    (function($){
-    $.fn.createPdf = function(parametros) {
-        
-        var config = {              
-            'fileName':'html-to-pdf'
-        };
-        
-        if (parametros){
-            $.extend(config, parametros);
-        }                            
-
-        var quotes = document.getElementById($(this).attr('id'));
-
-        html2canvas(quotes, {
-            onrendered: function(canvas) {
-                var pdf = new jsPDF('p', 'pt', 'letter');
-
-                for (var i = 0; i <= quotes.clientHeight/980; i++) {
-                    var srcImg  = canvas;
-                    var sX      = 0;
-                    var sY      = 980*i;
-                    var sWidth  = 900;
-                    var sHeight = 980;
-                    var dX      = 0;
-                    var dY      = 0;
-                    var dWidth  = 900;
-                    var dHeight = 980;
-
-                    window.onePageCanvas = document.createElement("canvas");
-                    onePageCanvas.setAttribute('width', 900);
-                    onePageCanvas.setAttribute('height', 980);
-                    var ctx = onePageCanvas.getContext('2d');
-                    ctx.drawImage(srcImg,sX,sY,sWidth,sHeight,dX,dY,dWidth,dHeight);
-
-                    var canvasDataURL = onePageCanvas.toDataURL("image/png", 1.0);
-                    var width         = onePageCanvas.width;
-                    var height        = onePageCanvas.clientHeight;
-
-                    if (i > 0) {
-                        pdf.addPage(612, 791);
-                    }
-
-                    pdf.setPage(i+1);
-                    pdf.addImage(canvasDataURL, 'PNG', 20, 40, (width*.62), (height*.62));
-                }
-
-                pdf.save(config.fileName);
-            }
-        });
-    };
-})(jQuery);
-
-function createPDF() {
-    $('#tbl_ligacao').createPdf({
-        'fileName' : 'testePDF'
+<script>
+var doc = new jsPDF({
+        orientation: 'l',
+        unit: 'pt',
+        format: [1000, 2200]
     });
-};
+    $('#idPDF').click(function () {
+        doc.fromHTML($('#divTable').html(), 30, 30, {
+            'width': 100,
+        });
+        doc.save('TabelaLigacoes.pdf');
+    }); 
 </script>
 
 </body>
